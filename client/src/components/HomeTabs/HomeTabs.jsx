080e73de56f63db4458dd58e982a933a561ffcfa
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2024-02-15 15:47:10 -0500

    finish HomeTabs component and styles

---
+++ client/src/components/HomeTabs/HomeTabs.jsx
@@ -0,0 +1,31 @@
+import "./hometabs.scss";
+
+const HomeTabs = () => {
+    return (
+        <nav className="homeTabs">
+            <ul>
+                <li className="selected">All</li>
+                <li>Trailers</li>
+                <li>Sci-fi films</li>
+                <li>Fandango Movieclips</li>
+                <li>Gaming</li>
+                <li>React routers</li>
+                <li>Dramedy</li>
+                <li>Live</li>
+                <li>Algorithms</li>
+                <li>Computes</li>
+                <li>Gadgets</li>
+                <li>Smartphones</li>
+                <li>NBA</li>
+                <li>Interior design</li>
+                <li>Sitcoms</li>
+                <li>Game Shows</li>
+                <li>Food</li>
+                <li>Invincible</li>
+                <li>Recently Uploaded</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default HomeTabs;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import "./home.scss";
 import axios from 'axios';
 import { homeData } from "../../homeData.js";
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import HomeTabs from "../../components/HomeTabs/HomeTabs.jsx";
 
 const Home = () => {
 
     return (
         <div className="home">
             <Sidebar />
+            <div className="mainPage">
+                <HomeTabs />
+            </div>
         </div>
     )
 }
