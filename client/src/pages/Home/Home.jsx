2710a4cc741623a0054e0e3048aae0259ce7ee71
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2024-02-18 07:46:46 -0500

    Uncomment test data and replace with request

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,30 +4,26 @@ import { homeRequest } from "./homeOptions.js";
 import { useQuery } from "@tanstack/react-query";
 import HomeTabs from "../../components/HomeTabs/HomeTabs.jsx";
 import Video from "../../components/Video/Video.jsx";
-import { homeData } from "../../homeData.js";
 
 const Home = () => {
 
-    // const { isLoading, isError, data } = useQuery({
-    //     queryKey: ["videos"],
-    //     queryFn: async () => await axios.request(homeRequest)
-    //         .then(res => {
-    //             return res.data;
-    //         })
-    //         .catch(err => console.error(err)),
-    //     staleTime: Infinity,
-    // })
+    const { isLoading, isError, data } = useQuery({
+        queryKey: ["videos"],
+        queryFn: async () => await axios.request(homeRequest)
+            .then(res => {
+                return res.data;
+            })
+            .catch(err => console.error(err)),
+        staleTime: Infinity,
+    })
 
     return (
         <div className="home">
             <HomeTabs />
-            {/* <div className="videos">
+            <div className="videos">
                 {isLoading
                 ? "Loading"
                 : data.contents.map((video) => <Video data={video} key={video.video.videoId} />)}
-            </div> */}
-            <div className="videos">
-                {homeData.map((video) => <Video data={video} key={video.videoId} />)}
             </div>
         </div>
     )
