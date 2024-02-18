3ee8b27d58cad0b02e65b2e92751a153502b016b
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2024-02-18 10:10:45 -0500

    make HomeTabs stay in position on tablet width

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,26 +4,28 @@ import { homeRequest } from "./homeOptions.js";
 import { useQuery } from "@tanstack/react-query";
 import HomeTabs from "../../components/HomeTabs/HomeTabs.jsx";
 import Video from "../../components/Video/Video.jsx";
+import { homeData } from "../../homeData.js";
 
 const Home = () => {
 
-    const { isLoading, isError, data } = useQuery({
-        queryKey: ["videos"],
-        queryFn: async () => await axios.request(homeRequest)
-            .then(res => {
-                return res.data;
-            })
-            .catch(err => console.error(err)),
-        staleTime: Infinity,
-    })
+    // const { isLoading, isError, data } = useQuery({
+    //     queryKey: ["videos"],
+    //     queryFn: async () => await axios.request(homeRequest)
+    //         .then(res => {
+    //             return res.data;
+    //         })
+    //         .catch(err => console.error(err)),
+    //     staleTime: Infinity,
+    // })
 
     return (
         <div className="home">
             <HomeTabs />
             <div className="videos">
-                {isLoading
+                {/* {isLoading
                 ? "Loading"
-                : data.contents.map((video) => <Video data={video} key={video.video.videoId} />)}
+                : data.contents.map((video) => <Video data={video} key={video.video.videoId} />)} */}
+                {homeData.map((video) => <Video data={video} key={video.videoId} />)}
             </div>
         </div>
     )
