a0c0561f856b46227e58e0f7955df719849382cd
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2024-02-20 12:48:54 -0500

    change homeOptions to makeRequest in makeRequest make request have baseUrl to be used everywhere

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,31 +1,32 @@
 import "./home.scss";
-import axios from 'axios';
-import { homeRequest } from "./homeOptions.js";
+import { makeRequest } from "./makeRequest.js";
 import { useQuery } from "@tanstack/react-query";
 import HomeTabs from "../../components/HomeTabs/HomeTabs.jsx";
 import Video from "../../components/Video/Video.jsx";
 import { homeData } from "../../homeData.js";
+import { useState } from "react";
 
 const Home = () => {
+    const [videos, setVideos] = useState({});
+    let cursor = "";
 
-    // const { isLoading, isError, data } = useQuery({
-    //     queryKey: ["videos"],
-    //     queryFn: async () => await axios.request(homeRequest)
-    //         .then(res => {
-    //             return res.data;
-    //         })
-    //         .catch(err => console.error(err)),
-    //     staleTime: Infinity,
-    // })
+    const { isLoading, isError, data } = useQuery({
+        queryKey: ["videos"],
+        queryFn: async () => await makeRequest("/home/")
+            .then(res => {
+                return res.data;
+            })
+            .catch(err => console.error(err)),
+        staleTime: Infinity,
+    })
 
     return (
         <div className="home">
             <HomeTabs />
             <div className="videos">
-                {/* {isLoading
+                {isLoading
                 ? "Loading"
-                : data.contents.map((video) => <Video data={video} key={video.video.videoId} />)} */}
-                {homeData.map((video) => <Video data={video} key={video.video.videoId} />)}
+                : data.contents.map((video) => <Video data={video} key={video.video.videoId} />)}
             </div>
         </div>
     )
--- client/src/pages/Home/makeRequest.js
+++ client/src/pages/Home/makeRequest.js
@@ -1,9 +1,11 @@
-export const homeRequest = {
+import axios from "axios";
+
+export const makeRequest = axios.create({
     method: 'GET',
-    url: 'https://youtube138.p.rapidapi.com/home/',
+    baseURL: 'https://youtube138.p.rapidapi.com',
     params: {hl: 'en', gl: 'US'},
     headers: {
       'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
       'X-RapidAPI-Host': import.meta.env.VITE_API_HOST
     }
-};
+});
